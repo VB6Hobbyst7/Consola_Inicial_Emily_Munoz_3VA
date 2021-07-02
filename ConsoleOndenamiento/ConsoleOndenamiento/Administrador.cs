71a1fe55f1c401b04c2cb04fda328d550a1d9709
ema-munoz <ema.munoz@yavirac.edu.ec>
2021-07-01 19:02:53 -0500

    Ordenamiento

---
+++ ConsoleOndenamiento/ConsoleOndenamiento/Administrador.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleOrdenamiento
+{
+    partial class Program
+    {
+        class Administrador
+        {
+            public int[] Ordenar(int[] desordenados)
+            {
+                for (int i = 0; i < desordenados.Length - 1; i++)
+                {
+                    int posicionMenor = ObtenerPosicionDelMenor(desordenados, i);
+                    var intercambio = desordenados[i];
+                    desordenados[i] = desordenados[posicionMenor];
+                    desordenados[posicionMenor] = intercambio;
+                }
+                return desordenados;                
+            }
+
+            private  int ObtenerPosicionDelMenor(int[] desordenados, int inicio)
+            {
+                int posicionMenor = inicio;
+                for (int posicionActual = inicio; posicionActual < desordenados.Length; posicionActual++)
+                    if (desordenados[posicionMenor] < desordenados[posicionActual])
+                        posicionMenor = posicionActual;
+                return posicionMenor;
+            }
+        }
+    }
+}
---
+++ ConsoleOndenamiento/ConsoleOndenamiento/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleOrdenamiento
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadElementos = 6;
+            var edificio = new int[cantidadElementos];
+            edificio[0] = 42987;
+            edificio[1] = 324;
+            edificio[2] = 654;
+            edificio[3] = 8;
+            edificio[4] = 45;
+            edificio[5] = -23;
+
+            Console.WriteLine(edificio[0]);
+
+            var administrador = new Administrador();
+            int[] ordenado = administrador.Ordenar(edificio);
+
+            Console.WriteLine("Hecho con FOR");
+            for (int i = 0; i < edificio.Length; i++)
+                Console.WriteLine(edificio[i]);
+
+            Console.WriteLine("\nHecho con FOREACH");
+            foreach (var actual in edificio)
+                Console.WriteLine(actual);
+        }
+    }
+}
