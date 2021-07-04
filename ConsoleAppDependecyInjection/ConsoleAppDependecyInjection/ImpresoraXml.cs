669516c1324a9b065c33cc24534c247e3fd5aed1
ema-munoz <ema.munoz@yavirac.edu.ec>
2021-07-04 12:13:40 -0500

    Dependency Injection

---
+++ ConsoleAppDependecyInjection/ConsoleAppDependecyInjection/ImpresoraXml.cs
@@ -0,0 +1,21 @@
+﻿using Abstraccion;
+using System.IO;
+using System.Text;
+
+namespace ConsoleAppDependecyInjection
+{
+    class ImpresoraXml : Impresora
+    {
+        public override string Imprimir(Libro libro, string ruta)
+        {
+            var archivo = $"{ruta}\\{libro.Titulo.Replace(" ", string.Empty)}.xml";
+            FileStream fileStream = File.Create(archivo);
+            var contenido = $"<id>{libro.LibroId}</id>\n<título>{libro.Titulo}<título>\n<autor>{libro.Autor}</autor>\n ";
+            byte[] buffer = Encoding.UTF8.GetBytes(contenido);
+            fileStream.Write(buffer);
+            fileStream.Flush();
+            fileStream.Close();
+            return archivo;
+        }
+    }
+}
