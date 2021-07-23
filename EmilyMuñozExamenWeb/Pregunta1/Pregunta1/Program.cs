44d37ffed4ea30533f40261e283eadc54e203c5b
ema-munoz <ema.munoz@yavirac.edu.ec>
2021-07-23 16:17:54 -0500

    Emily Muñoz Examen Web

---
+++ EmilyMuñozExamenWeb/Pregunta1/Pregunta1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Pregunta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] languages = { "Lisp", "ICI", "C#", "Angular", "F#" };
+
+            var result = string.Join(string.Empty, languages.Reverse().Select(s => s[0]));
+
+            Console.WriteLine (result);
+        }
+    }
+}
